e25895b0ffc2cc9ccdacd450a7da3efb158adf37
DeinKommandant <vortex.rieker@gmail.com>
2017-07-19 10:48:17 +0300

    VW

--- vaporwave/scheme/data/startmenu.php
+++ vaporwave/scheme/data/startmenu.php
@@ -1,50 +1,56 @@
-<ul class="dropdown-menu startmenu">
-			      <li class="nav_dropright">
-			        <a class="test" tabindex="-1" href="#">CATEGORY 1 <span class="glyphicon glyphicon-triangle-right"></span></a>
-			        <ul class="nav_dropright_menu">
-				        <li><a tabindex="-1" href="#">CAT 1 LIST 1</a></li>
-				        <li><a tabindex="-1" href="#">CAT 1 LIST 2</a></li>
-				        <li class="nav_dropright-level2">
-				          <a class="test" href="#">CAT 1 LIST 3 <span class="glyphicon glyphicon-triangle-right"></span></a>
-				            <ul class="nav_dropright_menu-level2">
-				            	<li><a id="1-3-1" onclick="articleToggler(this)" href="#">ITEM 1</a></li>
-				            	<li><a href="#">ITEM 2</a></li>
-				            	<li><a href="#">ITEM 3</a></li>
-				            	<li><a href="#">ITEM 4</a></li>
-				        	</ul>
-				        </li>
-				    </ul>
-			      </li>
-			      <li class="nav_dropright">
-			        <a class="test" tabindex="-1" href="#">CATEGORY 2 <span class="glyphicon glyphicon-triangle-right"></span></a>
-			        <ul class="nav_dropright_menu">
-				        <li><a tabindex="-1" href="#">CAT 2 LIST 1</a></li>
-				        <li><a tabindex="-1" href="#">CAT 2 LIST 2</a></li>
-				        <li class="nav_dropright-level2">
-				          <a class="test" href="#">CAT 2 LIST 3 <span class="glyphicon glyphicon-triangle-right"></span></a>
-				            <ul class="nav_dropright_menu-level2">
-				            	<li><a href="#">ITEM 1</a></li>
-				            	<li><a href="#">ITEM 2</a></li>
-				            	<li><a href="#">ITEM 3</a></li>
-				            	<li><a href="#">ITEM 4</a></li>
-				        	</ul>
-				        </li>
-				    </ul>
-			      </li>
-			      <li class="nav_dropright">
-			        <a class="test" tabindex="-1" href="#">CATEGORY 3 <span class="glyphicon glyphicon-triangle-right"></span></a>
-			        <ul class="nav_dropright_menu">
-				        <li><a tabindex="-1" href="#">CAT 3 LIST 1</a></li>
-				        <li><a tabindex="-1" href="#">CAT 3 LIST 2</a></li>
-				        <li class="nav_dropright-level2">
-				          <a class="test" href="#">CAT 3 LIST 3 <span class="glyphicon glyphicon-triangle-right"></span></a>
-				            <ul class="nav_dropright_menu-level2">
-				            	<li><a id="3-3-1" onclick="articleToggler(this)" href="#">ITEM 1</a></li>
-				            	<li><a href="#">ITEM 2</a></li>
-				            	<li><a href="#">ITEM 3</a></li>
-				            	<li><a href="#">ITEM 4</a></li>
-				        	</ul>
-				        </li>
-				    </ul>
-			      </li>
-			    </ul>
+<ul class="dropdown-menu startmenu box_2px">
+	<div id="left">
+		<div class="b">
+			Egor Nashilov
+		</div>
+	</div>
+	<li class="nav_dropright">
+		<a class="test" tabindex="-1" href="#"><img src="./content/folder_big.png"> Промо <span class="glyphicon glyphicon-triangle-right"></span></a>
+		<ul class="nav_dropright_menu">
+			<li class="nav_dropright-level2">
+				<a class="test" href="#">COUB <span class="glyphicon glyphicon-triangle-right"></span></a>
+				<ul class="nav_dropright_menu-level2">
+					<li><a id="1-1-1" onclick="articleToggler(this)" href="#">ITEM 1</a></li>
+					<li><a id="1-1-2" onclick="articleToggler(this)" href="#">ITEM 2</a></li>
+					<li><a id="1-1-3" onclick="articleToggler(this)" href="#">ITEM 3</a></li>
+					<li><a id="1-1-4" onclick="articleToggler(this)" href="#">ITEM 4</a></li>
+				</ul>
+			</li>
+		</ul>
+	</li>
+	<li class="nav_dropright">
+		<a class="test" tabindex="-1" href="#"><img src="./content/folder_big.png"> Рецензии <span class="glyphicon glyphicon-triangle-right"></span></a>
+		<ul class="nav_dropright_menu">
+			<li class="nav_dropright-level2">
+				<a class="test" href="#">Триколор ТВ <span class="glyphicon glyphicon-triangle-right"></span></a>
+				<ul class="nav_dropright_menu-level2">
+					<li><a id="2-1-1" onclick="articleToggler(this)" href="#">ITEM 1</a></li>
+					<li><a id="2-1-2" onclick="articleToggler(this)" href="#">ITEM 2</a></li>
+					<li><a id="2-1-3" onclick="articleToggler(this)" href="#">ITEM 3</a></li>
+					<li><a id="2-1-4" onclick="articleToggler(this)" href="#">ITEM 4</a></li>
+				</ul>
+			</li>
+		</ul>
+	</li>
+	<li class="nav_dropright">
+		<a class="test" tabindex="-1" href="#"><img src="./content/folder_big.png"> Новости <span class="glyphicon glyphicon-triangle-right"></span></a>
+		<ul class="nav_dropright_menu">
+			<li class="nav_dropright-level2">
+				<a class="test" href="#">Кинопоказ HD1 <span class="glyphicon glyphicon-triangle-right"></span></a>
+				<ul class="nav_dropright_menu-level2">
+					<li><a id="3-1-1" onclick="articleToggler(this)" href="#">ITEM 1</a></li>
+					<li><a id="3-1-2" onclick="articleToggler(this)" href="#">ITEM 2</a></li>
+					<li><a id="3-1-3" onclick="articleToggler(this)" href="#">ITEM 3</a></li>
+					<li><a id="3-1-4" onclick="articleToggler(this)" href="#">ITEM 4</a></li>
+				</ul>
+			</li>
+		</ul>
+	</li>
+	<li class="">
+		<a id="4-about" onclick="articleToggler(this)" href="#"><img src="./content/help_big.png"> Обо мне</a>
+	</li>
+	<li class="">
+		<a id="5-shut" onclick="articleToggler(this)" href="#"><img src="./content/shut_big.png"> Shut Down...</a>
+	</li>
+
+</ul>
---
+++ vaporwave/scheme/data/startmenu_.php
@@ -0,0 +1,50 @@
+<ul class="dropdown-menu startmenu">
+			      <li class="nav_dropright">
+			        <a class="test" tabindex="-1" href="#">CATEGORY 1 <span class="glyphicon glyphicon-triangle-right"></span></a>
+			        <ul class="nav_dropright_menu">
+				        <li><a tabindex="-1" href="#">CAT 1 LIST 1</a></li>
+				        <li><a tabindex="-1" href="#">CAT 1 LIST 2</a></li>
+				        <li class="nav_dropright-level2">
+				          <a class="test" href="#">CAT 1 LIST 3 <span class="glyphicon glyphicon-triangle-right"></span></a>
+				            <ul class="nav_dropright_menu-level2">
+				            	<li><a id="1-3-1" onclick="articleToggler(this)" href="#">ITEM 1</a></li>
+				            	<li><a href="#">ITEM 2</a></li>
+				            	<li><a href="#">ITEM 3</a></li>
+				            	<li><a href="#">ITEM 4</a></li>
+				        	</ul>
+				        </li>
+				    </ul>
+			      </li>
+			      <li class="nav_dropright">
+			        <a class="test" tabindex="-1" href="#">CATEGORY 2 <span class="glyphicon glyphicon-triangle-right"></span></a>
+			        <ul class="nav_dropright_menu">
+				        <li><a tabindex="-1" href="#">CAT 2 LIST 1</a></li>
+				        <li><a tabindex="-1" href="#">CAT 2 LIST 2</a></li>
+				        <li class="nav_dropright-level2">
+				          <a class="test" href="#">CAT 2 LIST 3 <span class="glyphicon glyphicon-triangle-right"></span></a>
+				            <ul class="nav_dropright_menu-level2">
+				            	<li><a href="#">ITEM 1</a></li>
+				            	<li><a href="#">ITEM 2</a></li>
+				            	<li><a href="#">ITEM 3</a></li>
+				            	<li><a href="#">ITEM 4</a></li>
+				        	</ul>
+				        </li>
+				    </ul>
+			      </li>
+			      <li class="nav_dropright">
+			        <a class="test" tabindex="-1" href="#">CATEGORY 3 <span class="glyphicon glyphicon-triangle-right"></span></a>
+			        <ul class="nav_dropright_menu">
+				        <li><a tabindex="-1" href="#">CAT 3 LIST 1</a></li>
+				        <li><a tabindex="-1" href="#">CAT 3 LIST 2</a></li>
+				        <li class="nav_dropright-level2">
+				          <a class="test" href="#">CAT 3 LIST 3 <span class="glyphicon glyphicon-triangle-right"></span></a>
+				            <ul class="nav_dropright_menu-level2">
+				            	<li><a id="3-3-1" onclick="articleToggler(this)" href="#">ITEM 1</a></li>
+				            	<li><a href="#">ITEM 2</a></li>
+				            	<li><a href="#">ITEM 3</a></li>
+				            	<li><a href="#">ITEM 4</a></li>
+				        	</ul>
+				        </li>
+				    </ul>
+			      </li>
+			    </ul>
